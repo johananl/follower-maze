7c0cc4c8e8dcb8e150c6e612e65f7eaa95ce476b
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-06-30 15:14:24 +0300

    Separate handling of events and client requests

--- main.go
+++ main.go
@@ -13,42 +13,65 @@ const (
 )
 
 func main() {
+	// Initialize event source listener
 	es, err := net.Listen("tcp", host+":"+eventSourcePort)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
+		fmt.Println("Error listening for events:", err.Error())
 		os.Exit(1)
 	}
 	defer es.Close()
 	fmt.Println("Listening for events on " + host + ":" + eventSourcePort)
 
+	// Initialize user clients listener
 	uc, err := net.Listen("tcp", host+":"+userClientsPort)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
+		fmt.Println("Error listening for clients:", err.Error())
 		os.Exit(1)
 	}
 	defer uc.Close()
 	fmt.Println("Listening for user clients on " + host + ":" + userClientsPort)
 
-	go acceptLoop(es)
-	acceptLoop(uc)
+	// Handle requests concurrently
+	go acceptEvents(es)
+	acceptClients(uc)
 }
 
-func acceptLoop(l net.Listener) {
+func acceptEvents(l net.Listener) {
 	for {
 		c, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting:", err.Error())
 		}
-		go handleRequest(c)
+		go handleEvent(c)
 	}
 }
 
-func handleRequest(conn net.Conn) {
+func acceptClients(l net.Listener) {
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting:", err.Error())
+		}
+		go handleClient(c)
+	}
+}
+
+func handleEvent(conn net.Conn) {
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		fmt.Println("Error reading event:", err.Error())
 	}
-	fmt.Println("Got a message:", string(buf))
+	fmt.Println("Got an event:", string(buf))
+	conn.Close()
+}
+
+func handleClient(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading client request:", err.Error())
+	}
+	fmt.Println("Got a client request:", string(buf))
 	conn.Close()
 }
