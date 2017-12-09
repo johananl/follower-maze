2cee9f9cc7d664ea3279dbc093faa362812a5c27
Johanan Liebermann <johanan.lieberman@gmail.com>
2017-12-09 02:44:05 +0100

    Add test for AcceptConnections

--- userclients/user-clients_test.go
+++ userclients/user-clients_test.go
@@ -1,8 +1,8 @@
 package userclients
 
 import (
-	"testing"
 	"net"
+	"testing"
 )
 
 var uh = NewUserHandler()
@@ -16,5 +16,33 @@ func TestRegisterUser(t *testing.T) {
 
 	if uh.Users[100] != conn {
 		t.Fatalf("User not registered")
+	}
+}
+
+// TestAcceptConnections ensures that AcceptConnections successfully returns net.Conn structs for TCP
+// connections received from a listener.
+func TestAcceptConnections(t *testing.T) {
+	l, err := net.Listen("tcp", "localhost:9099")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
+
+	ch := make(chan net.Conn)
+	go uh.AcceptConnections(l, ch)
+	cConn, err := net.Dial("tcp", "localhost:9099")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cConn.Close()
+
+	sConn := <-ch
+	defer sConn.Close()
+
+	if cConn.LocalAddr().String() != sConn.RemoteAddr().String() {
+		t.Fatalf(
+			"Invalid connection received: %v != %v",
+			cConn.LocalAddr().String(), sConn.RemoteAddr().String(),
+		)
 	}
 }
