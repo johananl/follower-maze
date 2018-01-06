d33f4c84af101d5a9c43e57f61ff873fe3042841
Johanan Liebermann <johanan.lieberman@gmail.com>
2018-01-06 18:57:07 +0100

    Handle SIGINT and shutdown gracefully

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
 
 	"bitbucket.org/johananl/follower-maze/events"
 	"bitbucket.org/johananl/follower-maze/userclients"
@@ -20,9 +22,16 @@ func main() {
 	// Initialize event handler
 	eh := events.NewEventHandler(qm, uh)
 
+	// Listen for SIGINT and shutdown gracefully
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	go func() {
+		<-shutdown
+		log.Println("SIGINT received - shutting down")
+		os.Exit(0)
+	}()
+
 	// Handle events and users concurrently (acceptUsers runs in the main goroutine)
 	go eh.Run()
 	uh.Run()
-
-	// TODO Handle graceful shutdown
 }
